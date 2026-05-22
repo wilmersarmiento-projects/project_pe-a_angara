574a7ad0dda88998b42902f3f99240c194d4ae17
wilmer sarmiento <wilmergafaro.sto@gmail.com>
2026-05-22 13:17:00 -0500

    Set assetPrefix in next.config.mjs
    Added assetPrefix to nextConfig for proper asset loading.

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: '/project_pe-a_angara',
+  assetPrefix: '/project_pe-a_angara/', // <--- Asegúrate de que tenga la barra final /
   images: {
     unoptimized: true,
   },
